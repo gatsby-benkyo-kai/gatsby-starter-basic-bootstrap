dd73ddc67cac636b8a4c7896bc1e3ae0249b2c6c
Hiroki-Fujishima <ucuc.as@gmail.com>
2021-03-26 00:19:30 +0900

    ページ作成途中です

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Col, Row, Crad} from "react-bootstrap";
+import layout from "../components/layout";
+import SEO from "../components/seo";
+import jigyo1Img from "../images/jigyo1.jpg";
+import jigyo2Img from "../images/jigyo2.jpg";
+import jigyo3Img from "../images/jigyo3.jpg";
+
+const JigyoPage=()=>(
+    
+)
