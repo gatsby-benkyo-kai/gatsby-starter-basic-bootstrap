648c0907d3f0a4ad122d380141b38842c997e529
Hiroki-Fujishima <ucuc.as@gmail.com>
2021-03-29 00:54:47 +0900

    jigyoページとcontactページ

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,6 +8,8 @@ exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/fujishimahiroki/benkyo_space/develop-space/gatsby-starter-basic-bootstrap/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/fujishimahiroki/benkyo_space/develop-space/gatsby-starter-basic-bootstrap/src/pages/404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/fujishimahiroki/benkyo_space/develop-space/gatsby-starter-basic-bootstrap/src/pages/about.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/fujishimahiroki/benkyo_space/develop-space/gatsby-starter-basic-bootstrap/src/pages/index.js")))
+  "component---src-pages-contact-js": hot(preferDefault(require("/Users/fujishimahiroki/benkyo_space/develop-space/gatsby-starter-basic-bootstrap/src/pages/contact.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/fujishimahiroki/benkyo_space/develop-space/gatsby-starter-basic-bootstrap/src/pages/index.js"))),
+  "component---src-pages-jigyo-js": hot(preferDefault(require("/Users/fujishimahiroki/benkyo_space/develop-space/gatsby-starter-basic-bootstrap/src/pages/jigyo.js")))
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,53 @@
 import React from "react";
-import {Col, Row, Crad} from "react-bootstrap";
-import layout from "../components/layout";
+import {Container, Col, Row, Form, Button } from "react-bootstrap";
+import Layout from "../components/layout";
 import SEO from "../components/seo";
-import jigyo1Img from "../images/jigyo1.jpg";
-import jigyo2Img from "../images/jigyo2.jpg";
-import jigyo3Img from "../images/jigyo3.jpg";
 
-const JigyoPage=()=>(
-    
-)
+const ContactPage =()=>(
+    <Layout>
+        <SEO title="お問い合わせ"/>
+        <Row>
+            <Col className="space"></Col>
+            </Row>
+            <Row>
+            <Col className="title-obi">
+            <h1 className="h1-font">お問い合わせ</h1>
+            </Col>
+            </Row>
+            <Row>
+            <Col className="space"></Col>
+            </Row>
+            <Container className="" style={{width:'80%'}}>
+            <Row>
+            <Col>
+            <p>(※このサンプルサイトで送信機能は使用できません)</p>
+            <Form>
+            <Form.Group controlId="formBasicText">
+            <Form.Control type="text" as="input" placeholder="お名前" name="name" required/>
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+            <Form.Control type="email" as="input" placeholder="メールアドレス" name="email" required />
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+            <Form.Control type="email" as="input" placeholder="メールアドレス" name="email" required/>
+
+            </Form.Group>
+            <Form.Group controlId="formTextarea">
+            <Form.Control as="textarea" placeholder="メッセージ" rows="3" name="massage" required/>
+            </Form.Group>
+            <Form.Group>
+                <Form.File id="FormControlFile" name="file" lang="ja" label="ファイル添付"/>
+                </Form.Group>
+                <Button variant="light" type="submit">送信</Button>
+            </Form>
+            </Col>
+            </Row>
+            </Container>
+            <Row>
+            <Col className="space"></Col>
+            </Row>
+            </Layout>
+            
+);
+
+export default ContactPage;
